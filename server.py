3ca65d0c8dead087562f3caf91f7eefafb892155
Harshit Joshi <harshitjoshi450@gmail.com>
2022-08-12 01:34:46 +0530

    added some more code and integrated guessing apis

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+from urllib import response
 from flask import Flask, render_template
 import random, datetime
+import requests
 
 app = Flask(__name__)
 
@@ -8,6 +10,18 @@ def main():
     random_num = random.randint(1, 10)
     current_year = int(datetime.datetime.now().year)
     return render_template("index.html", num=random_num, year=current_year)
+
+@app.route('/guess/<name>')
+def guess(name):
+    gender_url = f"https://api.genderize.io?name={name}"
+    gender_response = requests.get(gender_url)
+    gender_data = gender_response.json()
+    gender = gender_data["gender"]
+    age_url = f"https://api.agify.io?name={name}"
+    age_response = requests.get(age_url)
+    age_data =age_response.json()
+    age = age_data["age"]
+    return render_template("guess.html", user=name, gender=gender, age=age)
 
 if __name__ == "__main__":
     app.run(debug= True)
